932749b8f00b209abcdf68e52ce0d88fd3051775
AFFITABLE <91363484+AFFITABLE@users.noreply.github.com>
2025-03-17 12:55:34 +0530

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QUIZARTZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Testquiz.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QUIZARTZ.Models
+{
+    public class Testquiz
+    {
+
+        [Key]
+        public int QuestionId { get; set; }
+       
+
+        public string question { get; set; }
+        public string answer1 { get; set; }
+        public string answer2 { get; set; }
+        public string answer3 { get; set; }
+        public string answer4 { get; set; }
+        public string? rightans { get; set; }
+        public int score { get; set; }
+                
+
+    }
+}
